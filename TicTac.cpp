cd9f2641580907befbd075de86f5fc17e9f09403
farhanaSmrity <farhanasultanasmrity1@gmail.com>
2019-07-29 22:45:34 +0600

    isValidInput API implemented,showBox updated,play method refrator

--- TicTac.cpp
+++ TicTac.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include<windows.h>
 using namespace std;
 int grid[4][4]={32};
 
+
+// sign is set on that grid at the index
 void setGridIndex(int idx,int sign){
 	int row=idx/3,col=idx%3;
 	grid[row][col]=sign;
 }
 
+
+//it's return the value on that grid at the index
 int getGridIndex(int idx){
 	int row=idx/3,col=idx%3;
 	return grid[row][col];
 }
 
+
+bool isValidInput(int index){
+	
+	//range check (0-8)
+	if(!(index>=0  && index<9)) 
+	{
+		return false;
+	}
+	//overlap check
+	if(getGridIndex(index)==32)
+	{
+		return true;
+	}
+	
+}
 bool isWinner(int sign)
 {
 	if(grid[0][0]==grid[0][1] && grid[0][1]==grid[0][2] && grid[0][0]==sign ||\
@@ -55,40 +75,40 @@ Grid calculation
 
 void showbox()
 {
+	system("CLS");
+	printf("\n\n\n");
 	for(int i = 0;i<3;i++)
 	{
-		printf(" %c || %c || %c \n",getGridIndex(i*3),getGridIndex(i*3+1),getGridIndex(i*3+2));
+		
+		printf("\t\t\t\t\t\t %c || %c || %c \n",getGridIndex(i*3),getGridIndex(i*3+1),getGridIndex(i*3+2));
 		if(i<2){
-			printf("=============\n");
+			printf("\t\t\t\t\t\t=============\n");
 		}
 	}
 }
 //This method doesnt take any input. it returns the player who won the game or zero if its a tie.
 int play(){
-	int index,sign;
+	int index,sign,player;
 		for(int i=0;i<9;i++){
 			if(i%2==0){
-				//player 1
-				showbox();
-		    	scanf("%d",&index);
-			    setGridIndex(index,(int)'x');
-			    if(isWinner((int)'x')){
-			    	printf("player 1 wins!!!!.....");
-			    	return 1;
-				}
+				sign='x';
+				player=1;	
 	    	}
-	    	
 	    	else
 	    	{
-	    		//player 2
-	    		showbox();
-		    	scanf("%d",&index);
-			    setGridIndex(index,(int)'o');
-			    if(isWinner((int)'o')){
-			    	printf("player 2 wins!!!!.....");
-			    	return 2;
-				}
+	    		sign='o';
+	    		player=2;
 			}
+			do{
+				showbox();
+	    		printf("\n\t\t\t\t\tplayer%d put your input from 0-8:",player);
+		    	scanf("%d",&index);
+				setGridIndex(index,sign);
+				if(isWinner(sign)){
+			    	printf("player %d wins!!!!.....",player);
+			    return player;
+		    	}	
+			}while(1); //ffiixx  rrequired
 			
 		}
 }
